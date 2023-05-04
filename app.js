8830a255f4a263f123b738bf93c2c57930b36214
Ray625 <wdvgy32@gmail.com>
2023-05-04 19:54:54 +0800

    feat: add record model

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
-require('./config/mongoose')
 const app = express()
 
+require('./config/mongoose')
+const Record = require('./models/record')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
