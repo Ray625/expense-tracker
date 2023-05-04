2e42eee18213b6efb448690a8eaed9ebcb6a266e
Ray625 <wdvgy32@gmail.com>
2023-05-04 19:45:52 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+require('./config/mongoose')
 const app = express()
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
