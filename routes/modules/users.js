3c0bf5d9e9158be11c3508dfe2206b3266470e54
Ray625 <wdvgy32@gmail.com>
2023-05-07 01:26:37 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,16 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  res.redirect('/')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.redirect('/users/login')
+})
+
 module.exports = router
