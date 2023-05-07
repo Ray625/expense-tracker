530fc8a9d0d60a205201b76ff4e8d9440c7b6523
Ray625 <wdvgy32@gmail.com>
2023-05-07 21:32:54 +0800

    add .env.example

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 
 app.use(routes)
 
-const port = 3000
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
